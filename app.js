23a6d9803d45109e23852469847e3e6f4b172bc5
Kasib <kasibabdullah8@gmail.com>
2023-01-22 12:57:33 -0500

    fixing with help on styling

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ async function getAdvice() {
   let resp = await fetch("https://api.adviceslip.com/advice")
   let slipObj = await resp.json()
   let advice = slipObj['slip']['advice']
-  console.log(advice)
+  let p = document.getElementById('advice-text')
+  p.innerHTML = advice
 }
 
-// loading api call 
+// loading api call to the html page on page load
 window.onload = getAdvice()
