61cd717244bb7eb935641c10bb23f21fd68b2cc1
Kasib <kasibabdullah8@gmail.com>
2023-01-22 21:54:57 -0500

    formatted svg

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ async function getAdvice() {
   let slipObj = await resp.json()
   let advice = slipObj['slip']['advice']
   let p = document.getElementById('advice-text')
-  p.innerHTML = advice
+  p.innerHTML = `"${advice}"`
 }
 
 // loading api call to the html page on page load
