d0ba9af18e19b6181fb984e62dfa65b51c6e08ae
Kasib <kasibabdullah8@gmail.com>
2023-01-22 22:22:05 -0500

    completed the app

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ async function getAdvice() {
   let resp = await fetch("https://api.adviceslip.com/advice")
   let slipObj = await resp.json()
   let advice = slipObj['slip']['advice']
+  let adviceId = slipObj['slip']['id']
   let p = document.getElementById('advice-text')
+  let p_id = document.getElementById('advice-id')
   p.innerHTML = `"${advice}"`
+  p_id.innerHTML = `Advice #${adviceId}`
 }
 
 // loading api call to the html page on page load
 window.onload = getAdvice()
+
+// selecting a class, passing the function to a click of the button
+let dice = document.querySelector(".dice")
+dice.addEventListener("click", (getAdvice))
